d90e44b8db027eb46c3badc4dd77def3216ef4d0
Joshua Aaron Thomas <joshuaathomas36@students.abtech.edu>
2021-12-15 11:32:22 -0500

    the first vidoe complete

--- db_conn.php
+++ db_conn.php
@@ -1,9 +1,9 @@
 <?php
 
-$sname = '';
-$uname = '';
-$password = '';
-$db_name = '';
+$sname = 'localhost';
+$uname = 'asng12_images';
+$password = '1234';
+$db_name = 'asng12_images';
 
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 if(!$conn) {
--- index.php
+++ index.php
@@ -5,15 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Upload Using PHP</title>
-    <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            min-height: 100vh;
-        }
-    </style>
+    <link rel ="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
     <?php if (isset($_GET['error'])): ?>
